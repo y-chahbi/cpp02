db8af00cdbcf7997441f5d7b7809afb0fd243820
Youssef Chahbi <ychahbi@e2r1p4.1337.ma>
2023-12-15 17:23:10 +0100

    almost complet ex02 from cpp02

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 10:04:08 by ychahbi           #+#    #+#             */
+/*   Updated: 2023/12/15 16:58:59 by ychahbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+
+    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    return 0;
+}
