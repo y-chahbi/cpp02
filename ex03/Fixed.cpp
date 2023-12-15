52ebb55d6e86c9f6d8f75867a635f98e740c422e
Youssef Chahbi <ychahbi@e2r1p4.1337.ma>
2023-12-15 18:03:29 +0100

    todayDone

---
+++ ex03/Fixed.cpp
@@ -0,0 +1,185 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 10:03:43 by ychahbi           #+#    #+#             */
+/*   Updated: 2023/12/15 16:57:38 by ychahbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int const Fixed::i = 8;
+
+Fixed::Fixed()
+{
+    Raw = 0;
+}
+
+Fixed::Fixed(int const integer)
+{
+    Raw = integer * 256;
+}
+
+Fixed::Fixed(float const floatingPoint)
+{
+    Raw = roundf(floatingPoint * 256);
+}
+
+
+int    Fixed::getRawBits(void) const
+{
+    //std::cout << "getRawBits member function called" << std::endl;
+    return (Raw);
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    Raw = raw;
+}
+
+Fixed& Fixed::operator=(const Fixed& Copy)
+{
+    this->Raw = Copy.getRawBits();
+    return *this;
+}
+
+Fixed& Fixed::operator++(int)
+{
+    Fixed *tmp = new Fixed(*this);
+    ++this->Raw;
+    return (*tmp);
+}
+
+Fixed& Fixed::operator++()
+{
+    this->Raw++;
+    return (*this);
+}
+
+Fixed& Fixed::operator--(int)
+{
+    Fixed *tmp = new Fixed(*this);
+    --this->Raw;
+    return (*tmp);
+}
+
+Fixed& Fixed::operator--()
+{
+    this->Raw--;
+    return (*this);
+}
+
+int Fixed::toInt() const
+{
+    return (Raw / 256);
+}
+
+float Fixed::toFloat() const
+{
+    return (float) Raw / 256;
+}
+
+Fixed::Fixed(const Fixed& Copy)
+{
+    // setRawBits(Copy.getRawBits());
+    *this = Copy;
+    
+}
+
+Fixed& Fixed::operator*(const Fixed& Cpp) const
+{
+    Fixed *tmp = new Fixed(*this);
+    *tmp = this->toFloat() * Cpp.toFloat();
+    return (*tmp);
+}
+
+Fixed& Fixed::operator+(const Fixed& Cpp) const
+{
+    Fixed *tmp = new Fixed(*this);
+    *tmp = this->toFloat() + Cpp.toFloat();
+    return (*tmp);
+}
+
+Fixed& Fixed::operator-(const Fixed& Cpp) const
+{
+    Fixed *tmp = new Fixed(*this);
+    *tmp = this->toFloat() - Cpp.toFloat();
+    return (*tmp);
+}
+
+Fixed& Fixed::operator/(const Fixed& Cpp) const
+{
+    Fixed *tmp = new Fixed(*this);
+    *tmp = this->toFloat() / Cpp.toFloat();
+    return (*tmp);
+}
+
+int Fixed::operator>(const Fixed& Cpp) const
+{
+    if (this->toFloat() > Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+int Fixed::operator<(const Fixed& Cpp) const
+{
+    if (this->toFloat() < Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+int Fixed::operator>=(const Fixed& Cpp) const
+{
+    if (this->toFloat() >= Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+int Fixed::operator<=(const Fixed& Cpp) const
+{
+    if (this->toFloat() <= Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+int Fixed::operator==(const Fixed& Cpp) const
+{
+    if (this->toFloat() == Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+int Fixed::operator!=(const Fixed& Cpp) const
+{
+    if (this->toFloat() != Cpp.toFloat())
+        return (1);
+    return (0);
+}
+
+Fixed Fixed::max(Fixed& cd, const Fixed cdd)
+{
+    if (cd.toFloat() >= cdd.toFloat())
+        return (cd);
+    return (cdd);
+}
+
+Fixed Fixed::min(Fixed& cd, const Fixed cdd)
+{
+    if (cd.toFloat() <= cdd.toFloat())
+        return (cd);
+    return (cdd);
+}
+
+std::ostream & operator << (std::ostream &out, const Fixed &c)
+{
+    out << c.toFloat();
+    return out;
+}
+
+Fixed::~Fixed()
+{
+}
---
+++ ex03/Fixed.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychahbi <ychahbi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 10:03:55 by ychahbi           #+#    #+#             */
+/*   Updated: 2023/12/15 16:56:29 by ychahbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+class Fixed
+{
+    private:
+        int         Raw;
+        static  const int i;
+    public:
+        Fixed();
+        Fixed(int const integer);
+        Fixed(float const floatingPoint);
+        Fixed(const Fixed& Copy);
+        static Fixed max(Fixed& cd, const Fixed cdd);
+        static Fixed min(Fixed& cd, const Fixed cdd);
+        Fixed& operator=(const Fixed& Copy);
+        Fixed& operator++(int);
+        Fixed& operator++();
+        Fixed& operator--(int);
+        Fixed& operator--();
+        int operator>(const Fixed& Cpp) const;
+        int operator<(const Fixed& Cpp) const;
+        int operator>=(const Fixed& Cpp) const;
+        int operator<=(const Fixed& Cpp) const;
+        int operator==(const Fixed& Cpp) const;
+        int operator!=(const Fixed& Cpp) const;
+        Fixed& operator*(const Fixed& Cpp) const;
+        Fixed& operator+(const Fixed& Cpp) const;
+        Fixed& operator-(const Fixed& Cpp) const;
+        Fixed& operator/(const Fixed& Cpp) const;
+        void    setRawBits(int const raw);
+        int     getRawBits(void) const;
+        float   toFloat( void ) const;
+        int     toInt( void ) const;
+
+        ~Fixed();
+};
+
+std::ostream & operator << (std::ostream &out, const Fixed &c);
+
+#endif
